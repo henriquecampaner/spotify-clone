7963410e40abf8af6d199fcabc21ab2b8e0373ec
Henrique Campaner <henrique.campaner@live.com>
2022-05-06 11:36:08 +0100

    test random

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,7 +12,6 @@ interface LoginProps {
 }
 
 const Login = ({ providers }: LoginProps): ReactElement => {
-  console.log(Math.random());
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img src="https://links.papareact.com/9xl" alt="" className="w-52 mb-5" />
