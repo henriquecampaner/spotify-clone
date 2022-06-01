4a62dfd40de18ba081a5f9ef8c7bac7413bf00e0
Henrique <henrique.campaner@live.com>
2022-06-01 17:47:37 +0100

    add cypress

---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  viewportWidth: 1980,
+  viewportHeight: 1080,
+
+  env: {
+    TAGS: 'not @ignore',
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+  },
+});
